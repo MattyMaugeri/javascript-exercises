ecd1c480b5ce7dde13d22efb87f57e0e7a8ff697
Matthew Maugeri <53546314+MattyMaugeri@users.noreply.github.com>
2024-11-25 16:49:44 +1100

    completed exercises 2-6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...num) {
+    for (let i = 0; i < array.length; i++) {
+        for (let k = 0; k < num.length; k++) {
+            if (array.at(i) === num.at(k)) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    let result = 0;
+    if ((x !== parseInt(x)) ||
+        (y !== parseInt(y)) ||
+        (x < 0) ||
+        (y < 0)) {
+        return 'ERROR';
+    }
+    if (x > y) {
+        for (let i = y; i <= x; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = x; i <= y; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line

ff0bfc074268dd697fa6cab95d769e63aaa83079
Matthew Maugeri <53546314+MattyMaugeri@users.noreply.github.com>
2024-11-26 12:34:21 +1100

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let conversion = ((num - 32) * 5 / 9);
+  let rounded = Math.round(conversion * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let conversion = ((num * (9/5)) + 32);
+  let rounded = Math.round(conversion * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
